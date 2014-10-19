bb62cf539f29912bc96ecf602e7263e22b91f552
Dylan <buzzydd@hotmail.com>
2014-10-19 19:00:10 -0400

    Updated Class files
    I updated the Biome and WaterTypeClasses.  If you want to take a look
    and tell me what I can change or what is wrong with them that would be
    great.  I'll wait for some opinions before I create a Mock Frame to
    create the Biome and Water Types.

--- BiomeClass.java
+++ BiomeClass.java
@@ -3,19 +3,43 @@
  import java.lang.Object.*;
  import javax.swing.*;
  import java.lang.Exception;
+ import java.io.*;
 
  public class BiomeClass
  {
+	 	String biomeName;
  		float fishTempMax;
 	 	float fishTempMin;
 	 	float fishSaltMax;
 	 	float fishSaltMin;
 	 	float fishPHMax;
 	 	float fishPHMin;
-
-	 	BiomeClass()
-	 	{}
-
+	 	DataOutputStream dos;
+		FileOutputStream fos;
+		DataInputStream dis;
+		FileInputStream fis;
+		File fileName;
+		String dir;
+//Variables----------------------------------------------------------
+	 	BiomeClass(String nameOfBiome,String waterTypeOwner)
+	 	{
+			biomeName=nameOfBiome;
+			try
+			{
+				fileName=new File(biomeName);
+				dir=fileName.getAbsolutePath();
+				dir+="\\"+waterTypeOwner;
+				fileName=new File(dir+"\\"+biomeName+".dat");
+				fis=new FileInputStream(fileName);
+				dis=new DataInputStream(fis);
+				fos=new FileOutputStream(fileName);
+				dos=new DataOutputStream(fos);
+			}
+			catch(IOException ioe)
+			{
+			}
+		}
+//SetTemp Classes----------------------------------------------------
 	 	void setTempMax(float tempMax)
 	 	{
 	 		fishTempMax=tempMax;
@@ -24,6 +48,8 @@
 	 	{
 	 		fishTempMin=tempMin;
 	 	}
+//SetTemp Classes end------------------------------------------------
+//SetPH Classes------------------------------------------------------
 	 	void setPHMax(float phMax)
 	 	{
 	 		fishPHMax=phMax;
@@ -32,6 +58,8 @@
 	 	{
 	 		fishPHMin=phMin;
 	 	}
+//SetPH Classes end--------------------------------------------------
+//SetSalt Classes----------------------------------------------------
 	 	void setSaltMax(float saltMax)
 	 	{
 	 		fishSaltMax=saltMax;
@@ -40,4 +68,39 @@
 	 	{
 	 		fishSaltMin=saltMin;
 	 	}
+//SetSalt Classes end------------------------------------------------
+//SaveBiome----------------------------------------------------------
+	 	void saveBiome()
+	 	{
+			try
+			{
+				dos.writeFloat(fishTempMax);
+				dos.writeFloat(fishTempMin);
+				dos.writeFloat(fishPHMax);
+				dos.writeFloat(fishPHMin);
+				dos.writeFloat(fishSaltMax);
+				dos.writeFloat(fishSaltMin);
+			}
+			catch(IOException ioe)
+			{
+			}
+		}
+//SaveBiome end------------------------------------------------------
+//LoadBiome----------------------------------------------------------
+		void loadBiome()
+		{
+			try
+			{
+				fishTempMax=dis.readFloat();
+				fishTempMin=dis.readFloat();
+				fishPHMax=dis.readFloat();
+				fishPHMin=dis.readFloat();
+				fishSaltMax=dis.readFloat();
+				fishSaltMin=dis.readFloat();
+			}
+			catch(IOException ioe)
+			{
+			}
+		}
+//LoadBiome end------------------------------------------------------
  }
--- WaterTypeClass.java
+++ WaterTypeClass.java
@@ -3,11 +3,64 @@
  import java.lang.Object.*;
  import javax.swing.*;
  import java.lang.Exception;
+ import java.io.*;
 
  public class WaterTypeClass
  {
- 	BiomeClass biome;
-
- 	WaterTypeClass()
- 	{}
+ 	Vector<BiomeClass> biome;
+ 	DataOutputStream dos;
+	FileOutputStream fos;
+	DataInputStream dis;
+	FileInputStream fis;
+	String wtcName;
+	File fileName;
+	String dir;
+	int numberOfBiomes;
+//Beginning of WaterTypeClass()----------------------------------
+ 	WaterTypeClass(String waterTypeName)
+ 	{
+		wtcName=new String(waterTypeName);
+		fileName=new File(waterTypeName);
+		if(!fileName.exists())
+			fileName.mkdir();
+		dir=fileName.getAbsolutePath();
+		fileName=new File(dir+"\\"+waterTypeName+".dat");
+	}
+//End of WaterTypeClass()---------------------------------------
+//Beginning of setBiome()---------------------------------------
+	void setBiome(String biomeName)
+	{
+		biome.add(new BiomeClass(biomeName,wtcName));
+	}
+//End of setBiome()---------------------------------------------
+//Beginning of saveFile()---------------------------------------
+	void saveFile()
+		{
+			try
+				{
+					fos=new FileOutputStream(fileName);
+					dos=new DataOutputStream(fos);
+					dos.writeInt(biome.size());
+					for(int i=0;i<biome.size();i++)
+						dos.writeUTF(biome.get(i).toString());
+				}
+			catch(IOException ioe)
+			{}
+		}
+//End of saveFile()---------------------------------------------
+//Beginning of loadFile()---------------------------------------
+	void loadFile()
+		{
+			try
+				{
+					fis=new FileInputStream(fileName);
+					dis=new DataInputStream(fis);
+					numberOfBiomes=dis.readInt();
+					for(int i=0;i<numberOfBiomes;i++)
+						biome.add(new BiomeClass(dis.readUTF(),wtcName));
+				}
+			catch(IOException ioe)
+			{}
+		}
+//End of loadFile()---------------------------------------------
  }
